10d1619f74a4271f7f1e223be38c70dba2a3264b
jueeeeeen <jinchowchow2547@gmail.com>
2024-10-01 16:03:41 +0700

    half_hotel

--- 404_Hotel_Not_Found.py
+++ 404_Hotel_Not_Found.py
@@ -1,9 +1,13 @@
-class Node:
-    def __init__(self, data, left = None, right = None):
-        self.data = data
+class Room_Node:
+    def __init__(self, room_number, channel, left = None, right = None):
+        self.room_number = room_number
+        self.channel = {"plane": None, "ship": None, "train": None, "car": None}
         self.left = left if left is not None else None
         self.right = right if right is not None else None
         self.height = 0
+        
+    def __str__(self):
+        return str(self.data)
         
     def get_height(self, node):
         return -1 if node is None else node.height
@@ -14,75 +18,27 @@ class Node:
     def get_balance(self):
         return self.get_height(self.left) - self.get_height(self.right)
 
-class BST:
-    def __init__(self):
-        self.root = None
-        
-    def insert(self, data):
-        self.root = BST._insert(self.root, data)
-        
-    def _insert(node, data):
-        if node is None: return Node(data)
-        if int(data) < int(node.data):
-            node.left = BST._insert(node.left, data)
-        else:
-            node.right = BST._insert(node.right, data)
-        return node
-    
-    def get_successor(node):
-        cur = node.right
-        while cur and cur.left:
-            cur = cur.left
-        return cur    
-    
-    def delete(self, data):
-        self.root = BST._delete(self.root, data)
-    
-    def _delete(node, data):
-        if node is None: return None
-        if int(data) < int(node.data): node.left = BST._delete(node.left, data)
-        elif int(data) > int(node.data): node.right = BST._delete(node.right, data)
-        else:
-            if node.left is None: return node.right
-            elif node.right is None: return node.left
-            new_root = BST.get_successor(node)
-            node.data = new_root.data
-            node.right = BST._delete(node.right, new_root.data)
-        return node
-    
-    def print_tree(self):
-        BST._print_tree(self.root)
-        print()
-        
-    def _print_tree(node, level = 0):
-        if node is not None:
-            BST._print_tree(node.right, level+1)
-            print('    '*level + node.data )
-            BST._print_tree(node.left, level+1)
-    
 class AVL:
     def __init__(self):
         self.root = None
         
-    def insert(self, data):
-        self.root = AVL._insert(self.root, data)
+    def insert(self, room_number, channel):
+        self.root = AVL._insert(self.root, room_number, channel)
     
-    def _insert(node, data):
+    def _insert(node, room_number, channel):
         if node is None:
-            return Node(data)
-        if int(data) < int(node.data):
-            node.left = AVL._insert(node.left, data)
-        elif int(data) > int(node.data):
-            node.right = AVL._insert(node.right, data)
+            return Room_Node(room_number, channel)
+        if room_number < node.data:
+            node.left = AVL._insert(node.left, room_number, channel)
+        elif room_number > node.data:
+            node.right = AVL._insert(node.right, room_number, channel)
         else:
             return
             
         node.set_height()        
         new_root = AVL.rebalance(node)
         
-        if new_root:
-            return new_root
-        return node
+        return new_root if new_root else node
         
     def rebalance(node):
         balance = node.get_balance()
@@ -94,6 +50,12 @@ class AVL:
             if node.left.get_balance() == -1:
                 node.left = AVL.rotate_left(node.left)
             return AVL.rotate_right(node)
+        
+    def get_successor(root):
+        if root and root.left:
+            AVL.get_successor(root.left)
+        else:
+            return root
         
     def rotate_left(root):
         new_root = root.right
@@ -110,6 +72,32 @@ class AVL:
         root.set_height()
         new_root.set_height()
         return new_root
+    
+    def delete(self, data):
+        self.root = AVL._delete(self.root, data)
+        
+    def _delete(root, data):
+        if root is None: return None
+        if data < root.data: root.left = AVL._delete(root.left, data)
+        elif data > root.data: root.right = AVL._delete(root.right, data)
+        else:
+            if root.left is None: return root.right
+            elif root.right is None: return root.left
+            new_root = AVL.get_successor(root.right)
+            root.data = new_root.data
+            root.right = AVL._delete(root.right, new_root.data)
+        root.set_height()
+        new_root = AVL.rebalance(root)
+        return new_root if new_root else root
+    
+    def search(self, key):
+        return AVL._search(self.root, key)
+    
+    def _search(root, key):
+        if root is None: return None
+        if key == root.data: return str(root)
+        if key < root.data: return AVL._search(root.left, key)
+        return AVL._search(root.right, key)
         
     def print_tree(self):
         AVL._print_tree(self.root)
@@ -117,11 +105,55 @@ class AVL:
     def _print_tree(node, level = 0):
         if node is not None:
             AVL._print_tree(node.right, level+1)
-            print('    '*level + node.data )
+            print('    '*level + str(node) )
             AVL._print_tree(node.left, level+1)
             
-inp = input().split()
-tree = BST()
-for i in inp:
-    tree.insert(i)
-tree.print_tree()
+    def add_room(self, room_number):
+        self.insert(room_number)
+        
+    def assign_rooms(self):
+        plane = int(input("Enter number of plane : "))
+        ship = int(input("Enter number of ship : "))
+        train = int(input("Enter number of train : "))
+        car = int(input("Enter number of car : "))
+        passenger = int(input("Enter number of passenger : "))
+        for p in range(plane):
+            for s in range(ship):
+                for t in range(train):
+                    for c in range(car):
+                        for person in range(passenger):
+                            self.add_room((2**person) * (3**c) * (5**t) * (7**s) * (11**p))
+    
+    def show_room(self):
+        self.print_tree()
+        
+    def delete_room(self, room_number):
+        self.delete(room_number)
+        
+    def search_room(self, room_number):
+        print(self.search(room_number))
+
+hotel = AVL()                            
+print("------- Welcome to 404 Not Found Hotel -------")
+print("| Plane | Ship | Train | Car |")
+
+hotel.assign_rooms()
+
+while 1:
+    inp = input("| del | add | search | show | exit |\ntype an action : ")
+    if inp == "add":
+        room_number = int(input("-----Adding a room-----\nEnter a room number : "))
+        hotel.add_room(room_number)
+    elif inp == "del":
+        room_number = int(input("-----Deleting a room-----\nEnter a room number : "))
+        hotel.delete_room(room_number)
+    elif inp == "search":
+        room_number = int(input("-----Searching a room-----\nEnter a room number : "))
+        hotel.search_room(room_number)
+    elif inp == "show":
+        print("-----Showing rooms-----")
+        hotel.show_room()
+    elif inp == "exit":
+        exit()
+    else:
+        print("try again")
